a1aebce3159dba126a547ba7cd94931317b08e1b
Jens Maes <mail@ditisjens.be>
2022-02-22 10:28:37 +0100

    🖥️ Added 2 Active Directory custom sensors.

---
+++ Active Directory/AmountOfUsersWithExpiredPasswords.ps1
@@ -0,0 +1,7 @@
+﻿Import-Module ActiveDirectory
+
+$ExpiredPwd=Get-ADUser -filter * -properties Name, PasswordNeverExpires, PasswordExpired, PasswordLastSet | where {$_.Enabled -eq "True"} | where {$_.PasswordNeverExpires -eq $false} | where {$_.passwordexpired -eq $true}
+$cntExpiredPWD=@($ExpiredPWD.count)
+
+$x=[string]$cntExpiredPWD+":OK"
+write-output $x
---
+++ Active Directory/AmountOfUsersWithoutPasswordExpiration.ps1
@@ -0,0 +1,7 @@
+﻿Import-Module ActiveDirectory
+
+$NotExpiringPWD=Get-ADUser -Filter {Enabled -eq $True -and PasswordNeverExpires -eq $True -and objectCategory -eq "person" -and objectClass -eq "user"} -Properties sAMAccountName,DisplayName,LockedOut,LockoutTime,Enabled,AccountExpirationDate 
+$cntNotExpiringPWD=@($NotExpiringPWD.count)
+
+$x=[string]$cntNotExpiringPWD+":OK"
+write-output $x
